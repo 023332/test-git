fec42a7dfa1f7eb5b08e71e49d2bb38ac3290c67
Your Karen <your.nahapetyankaren422@gmail.com>
2025-02-14 02:37:09 +0400

    Added new changes in app.js and  added new file test.js

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
-console.log(1);
+import  {createServer} from "http";
+import fs from "fs";
+import path from "path";
+
+const mimeTypes  = {
+    ".html": "text/html",
+    ".css": "text/css",
+    ".js": "text/javascript"
+}
+
+function fileMiddleware(req, res, next) {
+    let url = req.url;
+    if(url === "/" ){
+        url = '/home.html';
+    }
+
+    const filePat = path.resolve("test"+url);
+    fs.promises.access(filePat)
+        .then(()=>{
+            path.extname(filePat);
+            res.writeHead(200, {"Content-Type": mimeTypes[next]});
+            fs.createReadStream(filePat).pipe(res);
+        })
+        .catch(() =>{
+            next();
+        })
+}
+
+const server = createServer((req, res) =>{
+
+    fileMiddleware(req, res, () =>{
+        if (req.url === "/hello"){
+            res.writeHead(200, {"Content-Type": "text/plain"});
+            res.end("Hello World!");
+        }else if(req.url === "/bye"){
+            res.writeHead(200, {"Content-Type": "application/json"});
+            res.end(JSON.stringify({
+                name: "john"
+            }))
+        }else {
+            res.writeHead(404, {"Content-Type": "text/plain"});
+            res.end("404 Not Found!");
+        }
+    });
+});
+
+server.listen(3001);
