813d91810d6d15cb1c3cc45634f2eff4c2d12ad5
Your Karen <your.nahapetyankaren422@gmail.com>
2025-02-14 17:45:54 +0400

    Added new changes in app.js and

--- app.js
+++ app.js
@@ -35,7 +35,8 @@ const server = createServer((req, res) =>{
         }else if(req.url === "/bye"){
             res.writeHead(200, {"Content-Type": "application/json"});
             res.end(JSON.stringify({
-                name: "john"
+                email: "nahapetyankaren422@gmail.com",
+                possword: "nahapetyan00"
             }))
         }else {
             res.writeHead(404, {"Content-Type": "text/plain"});
