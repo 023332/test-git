c2ed95c0f266ad31c2460727f5ed324541becfcd
Your Karen <your.nahapetyankaren422@gmail.com>
2025-02-17 17:21:43 +0400

    Your commit message

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ const server = createServer((req, res) =>{
             res.writeHead(200, {"Content-Type": "application/json"});
             res.end(JSON.stringify({
                 email: "nahapetyankaren422@gmail.com",
-                possword: "nahapetyan12"
+                possword: "nahapetyan112"
             }))
         }else {
             res.writeHead(404, {"Content-Type": "text/plain"});
